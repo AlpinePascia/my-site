eb4ba54d6c51242c8a667abe9f3bd083bfabcc94
Alexey Khodov <al.pascia@gmail.com>
2019-09-19 22:48:54 +0300

    Updates

--- src/components/ProfileImg/ProfileImg.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import profilePic from '../../media/profile-pic-2.jpeg';
-import './ProfileImg.scss';
-
-const ProfileImg = () => (
-  <img className="ProfileImg" src={profilePic} alt="Alex Xodov" />
-);
-
-export default ProfileImg;

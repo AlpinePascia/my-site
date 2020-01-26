9ab8fd76e3ac2f291269b32d48dfd034b3f141ad
Alexey Khodov <al.pascia@gmail.com>
2020-01-27 01:09:47 +0300

    Russian default

--- src/components/LanguageSwitch/LanguageSwitch.js
+++ src/components/LanguageSwitch/LanguageSwitch.js
@@ -6,26 +6,18 @@ import { useTranslation } from 'react-i18next';
 const LanguageSwitch = () => {
   const { i18n } = useTranslation();
 
-  // useEffect(() => {
-  //   if (i18n.language === 'en') {
-  //     document.title = 'kotiki';
-  //   } else if (i18n.language === 'ru') {
-  //     document.title = 'котики';
-  //   }
-  // }, [i18n.language]);
-
   return (
     <div className="LanguageSwitch">
-      <div
-        className={`LanguageLink${ i18n.language === 'en' ? ' active' : ''}`}
-        onClick={() => i18n.changeLanguage('en')}>
-        En
-      </div>
-      <div className="LanguageLinkDivider">|</div>
       <div
         className={`LanguageLink${ i18n.language === 'ru' ? ' active' : ''}`}
         onClick={() => i18n.changeLanguage('ru')}>
         Ru
+      </div>
+      <div className="LanguageLinkDivider">|</div>
+      <div
+        className={`LanguageLink${ i18n.language === 'en' ? ' active' : ''}`}
+        onClick={() => i18n.changeLanguage('en')}>
+        En
       </div>
     </div>
   );
